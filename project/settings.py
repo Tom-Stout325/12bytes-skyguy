dc3c35f9a3d5dbd613065652537ec78767bb2c0c
Tom <tom.stout325@gmail.com>
2025-07-09 20:25:56 -0400

    updated allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -15,7 +15,8 @@ ENV = env('ENV', default='development')
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
+
 
 # Installed Apps
 INSTALLED_APPS = [
